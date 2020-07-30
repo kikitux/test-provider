04fc84f46b334623250038c28964e39eff0e67a0
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2020-07-30 09:46:07 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    random  = "2.3.0"
+    tls     = "2.2.0"
+    azurerm = "2.20.0"
+  }
+}
+
+variable "myvar" {
+  default = "myvalue"
+}
+
+output "myvalue" {
+  value = var.myvar
+}
